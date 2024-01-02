4851431f34dc355bfaffc0d85c56c9d430d1c788
joonho lee <joonfluence.dev@gmail.com>
2024-01-03 00:12:05 +0900

    Chapter4 - 영화 예매 시스템

---
+++ app/src/main/java/object/_04_movie/movie/step02/DiscountConditionType.java
@@ -0,0 +1,7 @@
+package object._04_movie.movie.step02;
+
+public enum DiscountConditionType {
+    SEQUENCE,       // 순번조건
+    PERIOD          // 기간 조건
+}
+
---
+++ app/src/main/java/object/_04_movie/movie/step02/ReservationAgency.java
@@ -0,0 +1,10 @@
+package object._04_movie.movie.step02;
+
+import object._04_movie.money.Money;
+
+public class ReservationAgency {
+    public Reservation reserve(Screening screening, Customer customer, int audienceCount) {
+        Money fee = screening.calculateFee(audienceCount);
+        return new Reservation(customer, screening, fee, audienceCount);
+    }
+}
